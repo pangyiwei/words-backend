021febe2f9483101f02e15440f097face07cbc05
Yi Wei <pangyiwei@gmail.com>
2019-10-03 17:15:02 +0800

    Enable cost for localhost:3000

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const WORD_NUM = 30;
 
 let games = {};
 // Enable CORS for dev
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, 'public')));
